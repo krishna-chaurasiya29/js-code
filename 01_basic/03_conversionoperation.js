d38e4eb410b76b1b3ef4e4737a7c8fe3e0d3c3ba
krishna-chaurasiya29 <152362240+krishna-chaurasiya29@users.noreply.github.com>
2024-04-15 14:07:01 +0000

    new conversion

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,22 @@
+let score="33abc";
+let score2=null;
+let score3=undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(score2);
+console.log(score3);
+
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1 false=> 0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// ""=> false
+// "krishna"=> true
