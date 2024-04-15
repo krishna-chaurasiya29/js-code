6ea9d512db660f7ec509fffe6452f8267ccf5984
krishna-chaurasiya29 <152362240+krishna-chaurasiya29@users.noreply.github.com>
2024-04-15 13:49:48 +0000

    Learning the data types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)// will not work as we are using node js not an browser
+
+console.log(3+3);
+console.log("krishna");
+let age=18;
+let isLogeedIn =false
+let state;
+let name = "krishna"
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//null => standalone value
+//undefined =>
+//syombol => unique
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
