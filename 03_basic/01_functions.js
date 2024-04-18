e236c15d1029662a14e49203e2c37180e2278cc0
krishna-chaurasiya29 <152362240+krishna-chaurasiya29@users.noreply.github.com>
2024-04-18 18:29:45 +0000

    functions

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -19,3 +19,16 @@ function addNumbers(number1,number2)// in this we say that we are passing parame
 // addNumbers(4,7)//in this we are passing arguments we can also call values
 // addNumbers(3,"a")//output would be 3a
 
+function addTwoNumbers(a,b)
+{
+    let c=a+b;
+    return c;
+}
+const result1=addNumbers(5,9);
+const result2=addTwoNumbers(5,9);
+console.log("result: ",result1);
+console.log("result: ",result2);
+
+//in above function if we 
+
+//here result 1 comes undefine because we are not returning any thing and in function 2 we are returning thefore it gives values
