f951eac7a7b65e3c0d7f3a4852b3dc4e7cf75a71
krishna-chaurasiya29 <152362240+krishna-chaurasiya29@users.noreply.github.com>
2024-04-18 18:03:03 +0000

    json knowledge

---
+++ 02_basic/05_objects.js
@@ -0,0 +1,11 @@
+const course = {
+    coursename:"js ki mastery",
+    price:999,
+    courseInstructor:"KRishna Charasiya"
+}
+
+//course.courseInstructor
+
+const{courseInstructor:instructor} =course//given another name to course instructor
+// console.log(courseInstructor);
+console.log(instructor);
