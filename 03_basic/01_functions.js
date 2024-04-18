874a982bd0da69806c00b7bc0d1dfb164682dca9
krishna-chaurasiya29 <152362240+krishna-chaurasiya29@users.noreply.github.com>
2024-04-18 18:17:29 +0000

    basics of function

---
+++ 03_basic/01_functions.js
@@ -0,0 +1,21 @@
+//declaration of functions
+
+function myName()
+{
+    console.log("k");
+    console.log("r");
+    console.log("i");
+    console.log("s");
+    console.log("h");
+    console.log("n");
+    console.log("a");
+}
+// myName()//paranthesis is necessary to call the function else it will just as declared
+
+function addNumbers(number1,number2)// in this we say that we are passing parameters
+{
+    console.log(number1+number2);
+}
+// addNumbers(4,7)//in this we are passing arguments we can also call values
+// addNumbers(3,"a")//output would be 3a
+
