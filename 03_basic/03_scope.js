557323538f464b570234ba7cf285fe500908f04a
krishna-chaurasiya29 <152362240+krishna-chaurasiya29@users.noreply.github.com>
2024-04-20 07:03:55 +0000

    hoisting process explained in scope

---
+++ 03_basic/03_scope.js
@@ -0,0 +1,30 @@
+if(true)
+{
+    const username="krishna"
+    if(username ==="krishna");
+    {
+        const website ="youtube"
+        console.log(username + website);
+    }
+        // console.log(website);// not accessible
+
+}
+// console.log(username); //not accessible
+
+//conslole.log(username); not accessible 
+
+
+//**************INERESTING THINGS************ */
+
+console.log(addOne(5));
+function addOne(num) {
+    return num+1;
+}
+//heree it will not show any error because we are just decalring the function
+addTwo(4)//here it will show error as its not only declaring the error but it is also storing in some varibale
+const addTwo =function(num){
+    return num+2;
+}
+
+
+//this is the hoisting
