a1fedfe3da43828a4c29c81c86b66aecca0ba50a
krishna-chaurasiya29 <152362240+krishna-chaurasiya29@users.noreply.github.com>
2024-04-20 17:45:44 +0000

    some more about for in loop

---
+++ 04_ControlFlow/higherOrder2.js
@@ -0,0 +1,21 @@
+//Here will learn more about the higher loops concept
+
+const myObject={
+    js:'javascript',
+    cpp:'C++',
+    rb:'ruby',
+    swift:'swift by apple',
+}
+
+for (const key in myObject)
+{
+//  console.log(`${key} shortcut is for ${myObject[key]}`);
+        
+}
+const programming =["js","rb","java","cpp"]
+for(const key in programming)
+{
+    console.log(programming[key]);
+}
+
+//in prints the indices wheras the of prints the syntax
