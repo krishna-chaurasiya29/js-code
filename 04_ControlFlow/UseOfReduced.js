921f8b330bed12649898faab540876908e38b3be
krishna-chaurasiya29 <152362240+krishna-chaurasiya29@users.noreply.github.com>
2024-04-21 11:17:22 +0000

    use of reduced explain

---
+++ 04_ControlFlow/UseOfReduced.js
@@ -0,0 +1,33 @@
+//Reduced operator
+
+const myNums =[1,2,3]
+// const myTotal = myNums.reduce(function (acc,currival) {
+//     console.log(`acc: ${acc} and currival : ${currival}`);
+//     return acc + currival
+// },0)
+const myTotal =myNums.reduce( (acc,curr) => acc+curr ,0);
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
+//This is used to sum or perfoem many operation at a time
